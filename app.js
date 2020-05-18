6daaa7bce4f246505718e348b4aba4d007aba482
Julia W <w.julia91@gmail.com>
2020-05-18 15:38:05 +0200

    added code structure

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+// BUDGET CONTROLLER
 var budgetController = (function () {
     //CODE
 })();
 
+
+// UI CONTROLLER
 var UIContorller = (function () {
     // CODE
 })();
 
+
+// GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
     // CODE
 
