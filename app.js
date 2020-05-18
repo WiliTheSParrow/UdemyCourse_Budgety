8ec20ee21edb3b90295ad8efdc26805a4e99f138
Julia W <w.julia91@gmail.com>
2020-05-18 15:13:50 +0200

    added the other 2 modules

--- app.js
+++ app.js
@@ -6,7 +6,24 @@ var budgetController = (function () {
 
     return {
         publicTest: function (b) {
-            console.log(add(b));
+            return add(b);
         }
     }
 })();
+
+var UIContorller = (function () {
+
+    // CODE
+})();
+
+var controller = (function (budgetCtrl, UICtrl) {
+    var z = budgetController.publicTest(5);
+
+    // We don't have access to var z from the public, so we need to return another func to console.log it out.
+    return {
+        anotherPublic: function () {
+            console.log(z);
+        }
+    }
+
+})(budgetController, UIContorller);
