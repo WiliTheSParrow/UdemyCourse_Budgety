5cfcd9c195acc23ff218cb44726887f731fd7b79
Julia W <w.julia91@gmail.com>
2020-05-18 18:11:32 +0200

    UI controller module started

--- app.js
+++ app.js
@@ -5,16 +5,40 @@ var budgetController = (function () {
 
 
 // UI CONTROLLER
-var UIContorller = (function () {
-    // CODE
+var UIController = (function () {
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    };
 })();
 
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function () {
         // 1. Get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
 
         // 2. Add the item to the budget controller
 
@@ -24,10 +48,9 @@ var controller = (function (budgetCtrl, UICtrl) {
 
         // 5. Display the budget on the UI
 
-        console.log('it works');
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     // Keypress event
     // !!keyCode
@@ -40,4 +63,4 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     });
 
-})(budgetController, UIContorller);
+})(budgetController, UIController);
