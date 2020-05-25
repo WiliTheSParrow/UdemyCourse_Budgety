8db1b721391111aeb7d1db344f3f38964bbd56ef
Julia W <w.julia91@gmail.com>
2020-05-25 11:07:07 +0200

    updateBudget funct initiated and called

--- app.js
+++ app.js
@@ -151,6 +151,16 @@ var controller = (function (budgetCtrl, UICtrl) {
         });
     };
 
+    var updateBudget = function () {
+
+        // 1. Calculate the budget
+
+        // 2 Return the budget
+
+        // 3. Display the budget on the UI
+
+    };
+
     var ctrlAddItem = function () {
         var input, newItem;
 
@@ -166,9 +176,9 @@ var controller = (function (budgetCtrl, UICtrl) {
         // 4. Clear the fields
         UICtrl.clearFields();
 
-        // 4. Calculate the budget
+        // 5. Calculate and update budget
+        updateBudget();
 
-        // 5. Display the budget on the UI
 
     };
 
