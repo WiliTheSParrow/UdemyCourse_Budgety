94979ee10212b50deb69120b00bac138444f964b
Julia W <w.julia91@gmail.com>
2020-05-25 11:09:33 +0200

    input number is formatted to floating number

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ var UIController = (function () {
             return {
                 type: document.querySelector(DOMstrings.inputType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
 
