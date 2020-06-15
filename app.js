98925dfd93b87924acfd1e4f3de05763fb259841
Julia W <w.julia91@gmail.com>
2020-06-15 11:26:42 +0200

    year display works

--- app.js
+++ app.js
@@ -177,7 +177,8 @@ var UIController = (function () {
         expenseLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     };
 
     var formatNumber = function (num, type) {
@@ -296,7 +297,15 @@ var UIController = (function () {
             });
         },
 
+        displayMonth: function () {
+            var now, year;
 
+            now = new Date();
+            // var christmas = new Date(2016, 11,25);
+
+            year = now.getFullYear();
+            document.querySelector(DOMstrings.dateLabel).textContent = year;
+        },
 
 
         getDOMstrings: function () {
@@ -403,6 +412,7 @@ var controller = (function (budgetCtrl, UICtrl) {
     return {
         init: function () {
             console.log('App has started');
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
