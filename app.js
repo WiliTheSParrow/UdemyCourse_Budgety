5ada03db5c2f02156f5cbfaecc81d42648654d99
Julia W <w.julia91@gmail.com>
2020-06-15 10:39:27 +0200

    formatNumber private function

--- app.js
+++ app.js
@@ -180,6 +180,33 @@ var UIController = (function () {
         expensesPercLabel: '.item__percentage'
     };
 
+    var formatNumber = function (num, type) {
+        var numSplit, int, dec;
+        /*
+        + or - before number
+        exactly 2 decimal points
+        comma separating the thousands
+
+        2310.4567 -> + 2,310.46
+        2000 -> + 2,000.00
+        */
+
+        num = Math.abs(num);
+        num = num.toFixed(2);
+
+        numSplit = num.split('.');
+        int = numSplit[0];
+
+        if (int.length > 3) {
+            int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3); // input 23510, output 23,510
+        }
+
+        dec = numSplit[1];
+
+        return (type === 'exp' ? '-' : '+') + ' ' + int + dec;
+
+    };
+
     return {
         getInput: function () {
             return {
@@ -267,32 +294,7 @@ var UIController = (function () {
         },
 
 
-        formatNumber: function (num, type) {
-            var numSplit, int, dec;
-            /*
-            + or - before number
-            exactly 2 decimal points
-            comma separating the thousands
 
-            2310.4567 -> + 2,310.46
-            2000 -> + 2,000.00
-            */
-
-            num = Math.abs(num);
-            num = num.toFixed(2);
-
-            numSplit = num.split('.');
-            int = numSplit[0];
-
-            if (int.length > 3) {
-                int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3); // input 23510, output 23,510
-            }
-
-            dec = numSplit[1];
-
-            return (type === 'exp' ? '-' : '+') + ' ' + int + dec;
-
-        },
 
         getDOMstrings: function () {
             return DOMstrings;
